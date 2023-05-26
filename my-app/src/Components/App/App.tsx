e1bb0089952696dbd71c559a5958ed1c8b630a11
NeilBrooks <neil.brooks87@gmail.com>
2023-05-26 16:19:01 +0100

    CSS

--- my-app/src/Components/App/App.tsx
+++ my-app/src/Components/App/App.tsx
@@ -5,6 +5,7 @@ import WeatherData from '../WeatherData/Index';
 function App() {
   return (
     <div className="App">
+      <h1>Pick a city. Any City 🌏</h1>
       <WeatherData />
     </div>
   );
